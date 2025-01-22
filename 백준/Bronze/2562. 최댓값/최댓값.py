2b2ad000b043e51b52a9a756e844365be28ee471
jeehye lee <gnoss153@gmail.com>
2025-01-22 09:13:47 +0900

    [Bronze III] Title: 최댓값, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -0,0 +1,13 @@
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+e=int(input())
+f=int(input())
+g=int(input())
+h=int(input())
+i=int(input())
+numbers=[a,b,c,d,e,f,g,h,i]
+max_number=max(numbers)
+print(max_number)
+print(numbers.index(max_number)+1)
